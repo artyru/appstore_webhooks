a2451b506f5a0705201a77d434fea16f346177d5
Artem <artyru@gmail.com>
2026-07-14 12:39:37 +0200

    policy added

--- lib/generators/appstore_webhooks/install/templates/policies/dictionary_word_policy.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DictionaryWordPolicy < ApplicationPolicy
-  def generate?
-    AppstoreWebhooks::Entitlements::Checker.call(user: user, feature: :dictionary_words)
-  end
-end
